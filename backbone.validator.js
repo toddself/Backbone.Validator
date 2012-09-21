d6cf9d6a39c379515fa8df299b223b8609c6ac69
Todd Kennedy <todd@updater.com>
2012-09-21 11:49:19 -0400

    extra code to make length stuff work on strings

--- backbone.validator.js
+++ backbone.validator.js
@@ -23,11 +23,11 @@ Backbone.Validator = (function(){
                     validators.push(v);
                 }
             }
-            
+
         });
         return validators;
     };
-    
+
     var run_validators = function(value, validators, attribute){
         // call each validator in the order in which it was attached to the attribute
         // should an error be returned, we'll capture it and store it
@@ -40,7 +40,7 @@ Backbone.Validator = (function(){
         });
         return errors;
     };
-    
+
     var set_default = function(model, attr, errors, model_validators){
         // if the validation fails and the user wants to use the default that's been defined
         // we'll do that here.  We have to set {silent: true} to prevent a recursive call
@@ -56,8 +56,8 @@ Backbone.Validator = (function(){
         model.trigger('error', model, errors);
         return errors;
     };
-    
-    // we only want to bother with attributes that have changed so we don't 
+
+    // we only want to bother with attributes that have changed so we don't
     // revalidate valid data OR invalidate data that was passed in during
     // instantiation to get around model validation.  that sounds dumb, but
     // it might be necessary
@@ -72,11 +72,11 @@ Backbone.Validator = (function(){
         });
         return changedAttributes;
     };
-    
+
     return {
         // extend the model with these values.
         use_defaults: false,
-        
+
         validate: function(attrs, options) {
             var errors;
             var model = this;
@@ -87,16 +87,16 @@ Backbone.Validator = (function(){
                     if(_.isObject(model.validators[attr])){
                         var model_validators = get_validators(model, attr);
                         errors = run_validators(attrs[attr], model_validators, attr);
-                        if(errors.length > 0){  
+                        if(errors.length > 0){
                             if(model.use_defaults || attrs.use_defaults){
-                                set_default(model, attr, errors, model_validators);                         
+                                set_default(model, attr, errors, model_validators);
                             }
                         }
-                    }  
-                });             
+                    }
+                });
             }
             if(_.isArray(errors) && errors.length > 0){
-                return errors;                
+                return errors;
             }
         }
     };
@@ -112,7 +112,7 @@ Backbone.Validator.testers = (function(){
             return typeof !_.isUndefined(args[number]) ? args[number] : match;
         });
     };
-    
+
     return {
         // is the value in a given range
         range: function(value, range, attribute){
@@ -133,10 +133,10 @@ Backbone.Validator.testers = (function(){
                 if(typeof(value) !== type){
                     return format("Expected {0} to be of type {1} for {2} ", value, type, attribute);
                 }
-                
-            }            
+
+            }
         },
-        
+
         // does it match the given regex
         regex: function(value, re, attribute){
             var regex = new RegExp(re);
@@ -144,56 +144,62 @@ Backbone.Validator.testers = (function(){
                 return format("{0} did not match pattern {1} for {2}", value, regex.toString(), attribute);
             }
         },
-        
+
         // is the value in this list
         in_list: function(value, list, attribute){
             if(_.isArray(list) && _.indexOf(list, value) === -1){
                 return format("{0} is not part of [{1}] for {2}", value, list.join(', '), attribute);
             }
         },
-        
+
         // is the value a key
         is_key: function(value, obj, attribute){
             if(_.has(obj, value)){
                 return format("{0} is not one of [{1}] for {2}", value, _(obj).keys().join(', '), attribute);
             }
         },
-        
+
         // does the value come in under a max?
         max_length: function(value, length, attribute){
-            if(!_.isNull(value) && !_.isUndefined(value) && _.has(value, "length") && !_.isUndefined(value.length) && (value.length > length)){
-                return format("{0} is longer than {1} for {2} ", value, length, attribute);
+            if(!_.isNull(value) && !_.isUndefined(value)){
+                if((_.has(value, "length") && !_.isUndefined(value.length) && (value.length > length)) ||
+                   (_.isString(value) && (value.length > length))){
+                    return format('{0} is shorter than {1} for {2}', value, length, attribute);
+                }
             }
         },
-        
+
         // does the value meet a minimum requirement
         min_length: function(value, length, attribute){
-            if(!_.isNull(value) && !_.isUndefined(value) && _.has(value, "length") && !_.isUndefined(value.length) && (value.length < length)){
-                return format('{0} is shorter than {1} for {2}', value, length, attribute);
+            if(!_.isNull(value) && !_.isUndefined(value)){
+                if((_.has(value, "length") && !_.isUndefined(value.length) && (value.length < length)) ||
+                   (_.isString(value) && (value.length < length))){
+                    return format('{0} is shorter than {1} for {2}', value, length, attribute);
+                }
             }
         },
-        
+
         // does the value equal a default
         to_equal: function(value, example, attribute){
             if(!_.isEqual(value, example)){
                 return format("{0} is not the same as {1} for {2}", value, example, attribute);
             }
         },
-        
+
         // is the value at least a number
         min_value: function(value, limit, attribute){
             if(value < limit){
                 return format("{0} is smaller than {1} for {2}", value, limit, attribute);
             }
         },
-        
+
         // does the value exceed a number
         max_value: function(value, limit, attribute){
             if(value > limit){
                 return format("{0} exceeds {1} for {2}", value, limit, attribute);
             }
         },
-        
+
         // is this an instance of a particular object
         is_instance: function(value, type){
             if(!(value instanceof type)){
